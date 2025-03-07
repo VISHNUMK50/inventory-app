19f0397ceb848ee13ba01ac26556ba56e57e4f6c
VISHNU MK <vishnumk50@gmail.com>
2025-03-07 21:16:43 +0530

    Update ManageInventory.jsx

--- src/components/ManageInventory.jsx
+++ src/components/ManageInventory.jsx
@@ -31,7 +31,7 @@ const ManageInventory = () => {
   
   // State for GitHub configuration (same as in AddInventoryForm)
   const [githubConfig, setGithubConfig] = useState({
-    token: "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",
+    token: ${{ secrets.DATABASE_PAT }},
     repo: "inventory-app",
     owner: "VISHNUMK50",
     branch: "master",
