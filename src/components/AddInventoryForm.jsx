2eb2ef1e653be55562c67f48c6f39acc252d74d3
vishnu <vishnumk50@gmail.com>
2025-03-08 02:48:56 +0530

    testing

--- src/components/AddInventoryForm.jsx
+++ src/components/AddInventoryForm.jsx
@@ -59,7 +59,7 @@ const AddInventoryForm = () => {
     
     const [isSubmitting, setIsSubmitting] = useState(false);
     const [githubConfig, setGithubConfig] = useState({
-      token: process.env.REACT_APP_DATABASE_PAT,
+      token: process.env.NEXT_PUBLIC_DATABASE_PAT,
       repo: "inventory-app",
       owner: "VISHNUMK50",
       branch: "master",
