85abe7938a43a3e9802dd153825c09edbe487cd6
vishnu <vishnumk50@gmail.com>
2025-04-15 20:43:47 +0530

    s

--- src/config/githubConfig.js
+++ src/config/githubConfig.js
@@ -1,10 +1,12 @@
 // src/config/githubConfig.js
 const githubConfig = {
     token: process.env.NEXT_PUBLIC_DATABASE_PAT|| '',
-    repo: process.env.GITHUB_REPO ||"inventory-app",
+    // repo: process.env.GITHUB_REPO ||"inventory-app",
+    repo: process.env.GITHUB_REPO ||"inv-db",
     owner: process.env.GITHUB_OWNER ||"VISHNUMK50",
     branch: "master",
-    path: "database"
+    // path: "database"
+    path: "db"
   };
   
   export default githubConfig;
